8b0e648d49fbca0b80b39e09dfeded63b6d10435
FAIZAN\faiza <faizanmehmood3165@gmail.com>
2023-06-06 03:10:06 +0500

    quick fix in user schema file

--- models/user.js
+++ models/user.js
@@ -1,23 +1,17 @@
-import mongoose from 'mongoose';
-import passportLocalMongoose from 'passport-local-mongoose';
+import mongoose from "mongoose";
+import passportLocalMongoose from "passport-local-mongoose";
 
 const Schema = mongoose.Schema;
 
 const UserSchema = new Schema({
-	email: String,
-	password: String,
-	userName: String,
-	image: String,
-	posts: [
-		{
-			type: Schema.Types.ObjectId,
-			ref: 'Post'
-		}
-	]
+  email: String,
+  password: String,
+  userName: String,
+  image: String,
 });
 
 UserSchema.plugin(passportLocalMongoose);
 
-const userModel = mongoose.model('User', UserSchema);
+const userModel = mongoose.model("User", UserSchema);
 
 export default userModel;
